3ea85655a0098d8588a78bc111d55831c6bb852a
Snehil <snehilverma488@gmail.com>
2020-10-22 12:07:26 +0530

    [Snehil] Add : Creating queue using Linked List

--- DS-StackAndQueue/Program.cs
+++ DS-StackAndQueue/Program.cs
@@ -6,12 +6,19 @@ namespace DS_StackAndQueue
     {
         static void Main(string[] args)
         {
+            //Stack Operaions
             StackUsingLinkedList stack = new StackUsingLinkedList();
             stack.Push(70);
             stack.Push(30);
             stack.Push(56);
             stack.Display();
             stack.IsEmpty();
+            //Queue Operations
+            QueueUsingLinkedList queue = new QueueUsingLinkedList();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
         }
     }
 }
