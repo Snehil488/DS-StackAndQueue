290c1c3bcf5149872403e3af8e725eb2ccefe265
Snehil <snehilverma488@gmail.com>
2020-10-22 11:35:41 +0530

    [Snehil] Add : Created Stack Using Linked List

---
+++ DS-StackAndQueue/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DS_StackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackUsingLinkedList stack = new StackUsingLinkedList();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+        }
+    }
+}
---
+++ DS-StackAndQueue/StackUsingLinkedList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DS_StackAndQueue
+{
+    class StackUsingLinkedList
+    {
+        Node top;
+        public void Push(int data)
+        {
+            Node node = new Node(data);
+            if (top == null)
+            {
+                top = node;
+            }
+            else
+            {
+                node.next = top;
+                top = node;
+            }
+        }
+        public void Display()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+            }
+            else
+            {
+                Node n = top;
+                while (n.next != null)
+                {
+                    Console.Write(n.data + " ");
+                    n = n.next;
+                }
+                Console.Write(n.data + "\n");
+            }
+        }
+    }
+}
